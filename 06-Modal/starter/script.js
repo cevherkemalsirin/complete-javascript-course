5822366566c78cf736e7d619bd25f6c2132e9a8e
cevher kemal sirin <cevoyou@gmail.com>
2023-11-09 18:13:30 +0300

    Solve Modal Project

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtns = document.querySelectorAll(".show-modal");
+const closeModalBtn = document.querySelector(".close-modal");
+
+
+function ToggleOverlay()
+{
+    modal.classList.toggle("hidden");
+    overlay.classList.toggle("hidden");
+}
+
+
+for(let i = 0; i < openModalBtns.length; i++)
+{
+    openModalBtns[i].addEventListener("click",ToggleOverlay);
+}
+
+closeModalBtn.addEventListener("click", ToggleOverlay);
+overlay.addEventListener("click", ToggleOverlay);
+
+//keyboard events are globabl events so just use document
+/*
+keypress = holding
+keydown = press
+keyup = lift
+*/
+document.addEventListener("keydown", function(e) {
+    if(e.key === "Escape" && !overlay.classList.contains("hidden"))
+    {
+        ToggleOverlay();
+    }
+
+});
